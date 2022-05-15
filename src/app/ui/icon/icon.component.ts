a56d11888f7cb627bf6d825b7adf23ff7879d481
Gregor Tokarev <dev@gregortokarev.com>
2022-05-15 09:14:50 +0300

    Fix style issues

--- src/app/ui/icon/icon.component.ts
+++ src/app/ui/icon/icon.component.ts
@@ -7,6 +7,7 @@ import { Component, Input, OnInit } from '@angular/core'
 })
 export class IconComponent implements OnInit {
   @Input() iconName!: string
+  @Input() iconSize!: string
 
   ngOnInit(): void {}
 }
